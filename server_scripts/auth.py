d67cfb573a1470d284b9fb32abc390adaba1fea3
Kevin Dennis <kevindennis@mail.usf.edu>
2019-11-04 07:43:03 -0500

    Added session for project_name

--- server_scripts/auth.py
+++ server_scripts/auth.py
@@ -1,4 +1,5 @@
 from flask_login import LoginManager, login_user, login_required, current_user
+from flask import session
 
 # User model
 class User():
@@ -28,6 +29,7 @@ class User():
 
 	def set_project(self, project_name):
 		# set current project user is working on
+		session["project_name"] = project_name
 		self.current_project_name = project_name
 
 	def get_project(self):
@@ -39,4 +41,10 @@ login_manager.login_view = 'login' # points to login webpage
 
 @login_manager.user_loader
 def user_loader(username):
-	return(User(username))
+	user = User(username)
+	try:
+		user.set_project(session["project_name"])
+	except:
+		# Not set yet
+		pass
+	return(user)
